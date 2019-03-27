9b8d8ff0da316899fda5ae89fd745da2c337a244
felipe <pipe9906@gmail.com>
2019-03-27 10:31:42 -0500

    HOLA PERROS

---
+++ acciones/conexion.php
@@ -0,0 +1,14 @@
+<?php
+
+
+		$host = "localhost";
+		$user = "root";
+		$pw = "";
+		$db = "Busapp";
+
+		$con = mysqli_connect($host, $user, $pw) or die ("No se pudo concetar a base de datos");
+		mysqli_select_db($con, $db)or die ("no se encontro la base de datos");
+		
+
+ 
+?>
